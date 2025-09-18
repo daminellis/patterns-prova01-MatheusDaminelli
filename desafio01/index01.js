53153683ad262da6f3afcab625ac34373efaf062
Matheus Augusto Daminelli <matheus.90422@alunosatc.edu.br>
2025-09-18 19:46:27 -0300

    adiciona prova

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,68 @@
-console.log("Hello, World! 001");
+class FabricaUI {
+    criarBotao() {
+      throw new Error("Método deve ser implementado");
+    }
+    
+    criarJanela() {
+      throw new Error("Método deve ser implementado");
+    }
+  }
+  
+  class FabricaUIClara extends FabricaUI {
+    criarBotao() {
+      return new BotaoClaro();
+    }
+    
+    criarJanela() {
+      return new JanelaClara();
+    }
+  }
+  
+  class FabricaUIEscura extends FabricaUI {
+    criarBotao() {
+      return new BotaoEscuro();
+    }
+    
+    criarJanela() {
+      return new JanelaEscura();
+    }
+  }
+  
+  class BotaoClaro {
+    renderizar() {
+      return "Botão branco criado";
+    }
+  }
+  
+  class BotaoEscuro {
+    renderizar() {
+      return "Botão preto criado";
+    }
+  }
+  
+  class JanelaClara {
+    renderizar() {
+      return "Janela clara aberta";
+    }
+  }
+  
+  class JanelaEscura {
+    renderizar() {
+      return "Janela escura aberta";
+    }
+  }
+  
+  const tema = "dark";
+  let fabrica;
+  
+  if (tema === "light") {
+    fabrica = new FabricaUIClara();
+  } else {
+    fabrica = new FabricaUIEscura();
+  }
+  
+  const botao = fabrica.criarBotao();
+  const janelaUI = fabrica.criarJanela();
+  
+  console.log(botao.renderizar());
+  console.log(janelaUI.renderizar());
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,24 @@
-console.log("Hello, World! 001");
+class Config {
+    constructor() {
+      if (Config.instance) {
+        return Config.instance;
+      }
+      this.configuracoes = {};
+      Config.instance = this;
+    }
+  
+    definirConfig(chave, valor) {
+      this.configuracoes[chave] = valor;
+    }
+  
+    obterConfig(chave) {
+      return this.configuracoes[chave];
+    }
+  }
+  
+  const c1 = new Config();
+  c1.definirConfig("idioma", "pt-BR");
+  
+  const c2 = new Config();
+  console.log(c2.obterConfig("idioma"));
+  console.log(c1 === c2); 
